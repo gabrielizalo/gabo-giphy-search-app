adf46847d36cc169eeab033772b212faa5b64581
Gabriel Porras <ghporras@gmail.com>
2023-01-25 09:47:46 -0500

    feat: 4. application development > 2. giphy api and fetching data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,29 @@
 import Head from 'next/head'
+import {useEffect} from "react";
 
-export default function Home() {
-  return (
-    <div className='container'>
-      <Head>
-        <title>Create Next App</title>
+export default function Home(initialData: any) {
+    useEffect(() => {
+        console.log(initialData);
+    })
 
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="/styles.css" />
-      </Head>
-      <h1>Gabo's Giphy Search App</h1>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <Head>
+                <title>Gabo's Giphy Search App</title>
+
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="stylesheet" href="/styles.css"/>
+            </Head>
+
+            <h1>Gabo's Giphy Search App</h1>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    let catGiphys = await fetch('https://api.giphy.com/v1/gifs/search?q=cats&api_key=process.env.GIPHY_API_KEY&limit=10');
+    catGiphys = await catGiphys.json();
+    return {
+        props: {cat: catGiphys}
+    }
 }
