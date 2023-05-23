f431cd904d1699b618bb6e8ad6f58292381ff054
Gabriel Porras <ghporras@gmail.com>
2023-05-22 23:57:10 -0500

    feat: 4. application development > 5. getting data after a page is loaded

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,39 +3,51 @@ import {useEffect, useState} from "react";
 
 export default function Home(initialData: any) {
     const [formInputs, setFormInputs] = useState({});
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("cats");
 
     useEffect(() => {
-        console.log(initialData);
-    })
+        setSearchResults(initialData.catGiphys.data);
+    }, [initialData])
 
     const handleInputs  = (event: any) => {
         let {name, value} = event.target;
         setFormInputs({...formInputs, [name]: value});
     }
 
-    const handleSubmit  = (event: any) => {
+    // const handleSubmit  = (event: any) => {
+    //     event.preventDefault();
+    //     console.log(formInputs.searchTerm);
+    // }
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log(formInputs.searchTerm);
+        const apiKey = process.env.NEXT_PUBLIC_GIPHY_API_KEY
+        let giphys = await fetch(`https://api.giphy.com/v1/gifs/search?q=${formInputs.searchTerm}&api_key=${apiKey}&limit=10`);
+        giphys = await giphys.json();
+        console.log("giphys: ", giphys);
+        setSearchTerm(formInputs.searchTerm);
+        setSearchResults(giphys.data);
     }
 
     return (
         <div className='container'>
             <Head>
-                <title>Gabo's Giphy Search App</title>
-
+                <title>Gabo&apos;s Giphy Search App</title>
                 <link rel="icon" href="/favicon.ico"/>
                 <link rel="stylesheet" href="/styles.css"/>
             </Head>
 
-            <h1>Gabo's Giphy Search App</h1>
+            <h1>Gabo&apos;s Giphy Search App</h1>
 
             <form onSubmit={handleSubmit}>
                 <input name="searchTerm" onChange={handleInputs} required type="text" />
                 <button>Search</button>
             </form>
 
+            <h2>Search results for: {searchTerm}</h2>
             <div className="giphy-search-results-grid">
-                {initialData.catGiphys.data.map((each, index) => {
+                {searchResults && searchResults.length > 0 && searchResults.map((each, index) => {
                     return (
                         <div key={index}>
                             <h3>{each.title}</h3>
